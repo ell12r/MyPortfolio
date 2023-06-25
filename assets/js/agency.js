a3c706bfd915b4594696bb222d4a6d4a30d0ffbe
Elliot Amponsah <108998445+ell12r@users.noreply.github.com>
2023-06-25 14:06:56 +0000

    okayy, So i'm almost done with my portfolio site and i can't wait to publish it

--- assets/js/agency.js
+++ assets/js/agency.js
@@ -1,8 +1,12 @@
-function toggleNavbar() {
+function toggleNavbar(x) {
   const navbar = document.querySelector('.navbar');
   if (navbar.classList.contains('collapsed')) {
-    navbar.classList.remove('collapsed');
+    navbar.classList.remove('collapsed'); 
+    
   } else {
     navbar.classList.add('collapsed');
+    
   }
+ x.classList.toggle("fa-xmark");
 }
+
