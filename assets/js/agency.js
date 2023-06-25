2605e7432f8a8a752f69d8cf1c2800f074b3740f
Elliot Amponsah <108998445+ell12r@users.noreply.github.com>
2023-06-25 18:40:39 +0000

    Wansyd

--- assets/js/agency.js
+++ assets/js/agency.js
@@ -10,3 +10,54 @@ function toggleNavbar(x) {
  x.classList.toggle("fa-xmark");
 }
 
+
+function callHero(){
+  const Hero = document.querySelector('.Hero')
+  const About = document.querySelector('.about');
+  const Services = document.querySelector('.Service');
+  const Portfolio = document.querySelector('.Portfolio');
+  const LeftOver = document.querySelector('.LeftOver')
+    
+    Hero.style.display = "unset"; 
+    LeftOver.style.display = "none";
+}
+
+function callServ(){
+  const Hero = document.querySelector('.Hero')
+  const About = document.querySelector('.about');
+  const Services = document.querySelector('.Service');
+  const Portfolio = document.querySelector('.Portfolio');
+  const LeftOver = document.querySelector('.LeftOver');
+
+  LeftOver.style.display = "unset";
+  Hero.style.display = "none"; 
+  About.style.display = "none";
+  Services.style.display = "unset";
+  Portfolio.style.display = "none";
+}
+function callAbout(){
+  const Hero = document.querySelector('.Hero')
+  const About = document.querySelector('.about');
+  const Services = document.querySelector('.Service');
+  const Portfolio = document.querySelector('.Portfolio');
+  const LeftOver = document.querySelector('.LeftOver');
+
+  LeftOver.style.display = "unset";
+  Hero.style.display = "none"; 
+  About.style.display = "unset";
+  Services.style.display = "none";
+  Portfolio.style.display = "none";
+}
+function callPort(){
+  const Hero = document.querySelector('.Hero')
+  const About = document.querySelector('.about');
+  const Services = document.querySelector('.Service');
+  const Portfolio = document.querySelector('.Portfolio');
+  const LeftOver = document.querySelector('.LeftOver');
+
+  LeftOver.style.display = "unset";
+  Hero.style.display = "none"; 
+  About.style.display = "none";
+  Services.style.display = "none";
+  Portfolio.style.display = "unset";
+}
